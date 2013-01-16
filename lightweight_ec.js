63ccdd351a71288f859870528486c18e45df62d0
Anthony Nardi <anthonygt@gmail.com>
2013-01-16 12:47:53 -0500

    Entity API uses prototypical inheritance. Still need to think about using prototypical inheritance when adding functionality to entities..

--- lightweight_ec.js
+++ lightweight_ec.js
@@ -1,65 +1,72 @@
 var ec = (function () {
 
-	var list = [],
-		returnObject = {};
+  var list = [],
+      returnObject = {};
 
-	var uid = (function () {
-		
-		var counter = 0;
+  var uid = (function () {
+	
+    var counter = 0;
 
-		return function () {
+    return function () {
 
-			return counter++;
-		
-		};
+      return counter++;
+	
+	};
 
-	}());
+  }());
 
-	var addComponent = function (component) {
+  var addComponent = function (component) {
 
-		for (property in component) {
+	for (property in component) {
 
-			if (!component.hasOwnProperty(property)) continue;
-		
-			this[property] = component[property];
+      if (!component.hasOwnProperty(property)) continue;
+	
+	  this[property] = component[property];
 
-		}
+	}
 
-		return this;
+	return this;
+
+  };
+
+  var removeComponent = function (component) {
+
+    for (property in component) {
+
+	  if (!component.hasOwnProperty(property) || !this.hasOwnProperty(property)) continue;
+
+	  delete this[property];
+
+	}
+
+	return this;
+
+  };
+
+  var createEntity = function () {
+
+    var entity = function () {};
+
+	entity.prototype.addComponent = addComponent;
+	entity.prototype.removeComponent = removeComponent;
+
+	return function () {
+
+	  var e = Object.create(entity.prototype);
+
+	  e.id = uid();
+	  list[e.id] = e;
+
+	  return e;
 
 	};
 
-	var removeComponent = function (component) {
+  };
 
-		for (property in component) {
+  returnObject = function () { return createEntity() };
 
-			if (!component.hasOwnProperty(property) || !this.hasOwnProperty(property)) continue;
+  returnObject.list = list;
 
-			delete this[property];
-
-		}
-
-		return this;
-
-	};
-
-	var createEntity = function () {
-
-		var entity = {};
-
-		entity.addComponent = addComponent;
-		entity.removeComponent = removeComponent;
-		entity.id = uid();
-		list[entity.id] = entity;
-
-		return entity;
-
-	};
-
-	returnObject = function () { return createEntity() };
-
-	returnObject.list = list;
-
-	return returnObject;
+  return returnObject;
 
 }());
